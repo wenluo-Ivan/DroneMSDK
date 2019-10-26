564974292e40700835bc0df3afef230ec03b96b6
李文烙 <liwl@hst.com>
2019-10-26 18:09:22 +0800

    改为无title模式

--- app/src/main/java/com/distance/third/dji/activity/DronePreviewActivity.java
+++ app/src/main/java/com/distance/third/dji/activity/DronePreviewActivity.java
@@ -1,12 +1,15 @@
 package com.distance.third.dji.activity;
 
-import android.app.Activity;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.FrameLayout;
 
 import com.distance.third.dji.R;
@@ -15,26 +18,35 @@ import com.distance.third.dji.model.DroneModel;
 import com.distance.third.dji.util.FileUtils;
 
 import java.io.File;
+import java.nio.ByteBuffer;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * @author 李文烙
  * @date 2019/10/26
  * @desc 无人机预览页面
  */
-public class DronePreviewActivity extends Activity implements DroneModel.DJIProductStateCallBack, DroneModel.DJIVideoDataCallBack {
+public class DronePreviewActivity extends AppCompatActivity implements DroneModel.DJIProductStateCallBack,
+        DroneModel.DroneH264VideoDataCallBack,DroneModel.DroneYUVDataCallBack, View.OnClickListener {
 
     private static final String TAG = "DJIPreviewActivity";
 
     @BindView(R.id.fl_dji_preview)
     FrameLayout videoPreviewFrameLayout;
+    @BindView(R.id.btn_shoot_pic)
+    Button btnShootPic;
+    @BindView(R.id.btn_surface_preview)
+    Button btnSurfacePreview;
     private SurfaceView videostreamPreviewSf;
+    private long lastUpdate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dji_preview);
+        ButterKnife.bind(this);
         initValues();
         initListeners();
     }
@@ -54,8 +66,6 @@ public class DronePreviewActivity extends Activity implements DroneModel.DJIProd
         layoutParams.height = ViewGroup.LayoutParams.MATCH_PARENT;
         layoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT;
         videostreamPreviewSf.setLayoutParams(layoutParams);
-        DroneModel.getInstance(this).setDjiVideoDataCallBack(this);
-
     }
 
     @Override
@@ -65,12 +75,18 @@ public class DronePreviewActivity extends Activity implements DroneModel.DJIProd
     }
 
     protected void initListeners() {
+        DroneModel.getInstance(this).setDroneYUVDataCallBack(this);
+        DroneModel.getInstance(this).setDjiVideoDataCallBack(this);
         DroneModel.getInstance(this).setDjiProductStateCallBack(this);
+        btnShootPic.setOnClickListener(this);
+        btnSurfacePreview.setOnClickListener(this);
     }
 
     @Override
     protected void onStop() {
         super.onStop();
+        DroneModel.getInstance(this).setDjiProductStateCallBack(null);
+        DroneModel.getInstance(this).setDroneYUVDataCallBack(null);
         DroneModel.getInstance(this).setDjiVideoDataCallBack(null);
         DroneModel.getInstance(this).unRegisterDJIVideoDataListener();
         if (videostreamPreviewSf != null) {
@@ -97,8 +113,37 @@ public class DronePreviewActivity extends Activity implements DroneModel.DJIProd
     @Override
     public void onReceviedVideoData(byte[] videoBuffer, int size) {
         Logger.info(TAG, "无人机数据回调：" + size);
-        long dblSampleTime = System.currentTimeMillis() / 1000;
         FileUtils.writeToLocal(Environment.getExternalStorageDirectory().getAbsolutePath() +
-                File.separator + "fsmeeting" + File.separator + "test.h264", videoBuffer);
+                File.separator + "drone" + File.separator + "test.h264", videoBuffer);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_shoot_pic:
+                if (btnShootPic.isSelected()) {
+                    btnShootPic.setSelected(false);
+                    DroneModel.getInstance(this).setReceviedYuvData(false);
+                } else {
+                    DroneModel.getInstance(this).setReceviedYuvData(true);
+                }
+                break;
+            case R.id.btn_surface_preview:
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onReceviedYUVData(ByteBuffer yuvFrame, int dataSize, int width, int height) {
+        if (System.currentTimeMillis() - lastUpdate > 1000) {
+            lastUpdate = System.currentTimeMillis();
+            Logger.debug(TAG, "onYuvDataReceived " + dataSize);
+            final byte[] bytes = new byte[dataSize];
+            yuvFrame.get(bytes);
+            Logger.debug(TAG, "onYuvDataReceived width: " + width + " ,height:" + height);
+            AsyncTask.execute(() -> FileUtils.saveYuvDataToJpeg(bytes, width, height));
+        }
     }
 }
